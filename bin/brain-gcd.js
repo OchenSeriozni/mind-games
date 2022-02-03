f31c106f13a1bdd8c5283a99ba10cb6cace57989
kosarev danila <kosarev0726@gmail.com>
2022-02-03 11:09:55 +0300

    Adding shebangs to game files

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node.
 import gameNode from '../game/code_game_node.js';
 
 console.log(gameNode());
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node.
 import gameProgression from '../game/code_game_progression.js';
 
 console.log(gameProgression());
