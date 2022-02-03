c217df3490ffb9f0aacff1a41ada546eb5f76dcf
kosarev danila <kosarev0726@gmail.com>
2022-02-03 11:21:44 +0300

    mod file

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node.
 import gameCalc from '../game/code_games_calc.js';
 
-console.log(gameCalc());
+gameCalc();
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node.
 import gameNode from '../game/code_game_node.js';
 
-console.log(gameNode());
+gameNode();
