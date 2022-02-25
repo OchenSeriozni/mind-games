6b111274dd3763e133d4370e9943b267e77391ee
kosarev danila <kosarev0726@gmail.com>
2022-02-25 16:49:36 +0300

    fix maxDivisor.js Changing the logic

--- src/maxDivisor.js
+++ src/maxDivisor.js
@@ -1,23 +1,8 @@
 const maxDivisor = (num1, num2) => {
-  if (num1 === 1 || num2 === 1) {
-    return 1;
-  }
-  if (num1 === 0 || num2 === 0) {
-    return 0;
-  }
-  let chek = 0;
-  if (num1 > num2) {
-    chek = num2;
-  }
-  chek = num1;
-  for (let i = chek - 1; i >= 1; i -= 1) {
-    if (i === 1) {
-      if (num1 % num2 !== 0 || num2 % num2 !== 0) {
-        return 1;
-      }
-      return chek;
-    }
-    if (num1 % i === 0 && num2 % i === 0) {
+  const minNum = num1 >= num2 ? num2 : num1;
+  const maxNum = num1 >= num2 ? num1 : num2;
+  for (let i = minNum; i >= 1; i -= 1) {
+    if (minNum % i === 0 && maxNum % i === 0) {
       return i;
     }
   }
